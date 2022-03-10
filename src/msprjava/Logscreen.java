d6d4ff9516c84b5102579dd84126a5037927c97c
nicolas cammareri <Nicolas.cammareri.nc@gmail.com>
2022-03-10 17:59:46 +0100

    Liste users avec redirection

--- src/msprjava/Logscreen.java
+++ src/msprjava/Logscreen.java
@@ -51,34 +51,20 @@ public class Logscreen extends Thread{
 "    <div class=\"limiter\">\n" +
 "        <div class=\"container-login100\">\n" +
 "            <div class=\"wrap-login100\">\n" +
-"                <form class=\"login100-form validate-form\" action=\"javascript:void(0);\">\n" +
-"                    <span class=\"login100-form-logo\">\n" +
-"                        <i class=\"bi bi-person-circle\"></i>\n" +
-"                    </span>\n" +
-"                    <br>\n" +
-"                    <span class=\"login100-form-title p-b-34 p-t-27\">\n" +
-"                        Connexion\n" +
-"                    </span>\n" +
+"                <span class=\"login100-form-title p-b-34 p-t-27\">\n" +
+"                    Utilisateurs\n" +
+"                </span><br>\n" +
 "\n" +
-"                    <br>\n" +
-"                    <div class=\"wrap-input100 validate-input\" data-validate = \"Entrer votre identifiant\">\n" +
-"                        <input class=\"input100\" type=\"text\" name=\"mail\" id=\"mail\" placeholder=\"Identifiant\">\n" +
-"                        <span class=\"focus-input100\" data-placeholder=\"\"> </span>\n" +
-"                    </div>\n" +
-"\n" +
-"                    <div class=\"wrap-input100 validate-input\" data-validate=\"Entrer votre mot de passe\">\n" +
-"                        <input class=\"input100\" type=\"password\" name=\"pass\" id=\"pass\" placeholder=\"Mot de passe\">\n" +
-"                        <span class=\"focus-input100\" data-placeholder=\"\"></span>\n" +
-"                    </div>\n" +
-"\n" +
-"\n" +
-"                    <div class=\"container-login100-form-btn\">\n" +
-"                        <button class=\"login100-form-btn\"  type=\"submit\" id=\"submitBtn\">\n" +
-"                            Se connecter\n" +
-"                        </button>\n" +
-"                    </div>\n" +
-"\n" +
-"                </form>\n" +
+"                <div class=\"list-group\">\n" +
+"                    <a href=\"comptes/cberthier/index.html\" class=\"list-group-item text-center list-group-item-action list-group-item-light\">Berthier</a>                    \n" +
+"                    <a href=\"comptes/mgontier/index.html\" class=\"list-group-item text-center list-group-item-action list-group-item-light\">Gontier</a>\n" +
+"                    <a href=\"comptes/smacky/index.html\" class=\"list-group-item text-center list-group-item-action list-group-item-light\">Macky</a>\n" +
+"                    <a href=\"comptes/mjackson/index.html\" class=\"list-group-item text-center list-group-item-action list-group-item-light\">Jackson</a>\n" +
+"                    <a href=\"comptes/rmoreno/index.html\" class=\"list-group-item text-center list-group-item-action list-group-item-light\">Moreno</a>\n" +
+"                    <a href=\"comptes/bfa/index.html\" class=\"list-group-item text-center list-group-item-action list-group-item-light\">Fa</a>\n" +
+"                    <a href=\"comptes/mhollande/index.html\" class=\"list-group-item text-center list-group-item-action list-group-item-light\">Hollande</a>\n" +
+"                    <a href=\"comptes/palois/index.html\" class=\"list-group-item text-center list-group-item-action list-group-item-light\">Alois</a>\n" +
+"                  </div>\n" +
 "            </div>\n" +
 "        </div>\n" +
 "    </div>\n" +
@@ -88,57 +74,7 @@ public class Logscreen extends Thread{
 "<!-- Authentification  -->\n" +
 "<script src=\"https://code.jquery.com/jquery-3.6.0.js\" integrity=\"sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=\" crossorigin=\"anonymous\"></script>\n" +
 "<script src=\"https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js\" crossorigin=\"anonymous\"></script>\n" +
-"<script src=\"main.js\"></script>\n" +
-"<script>\n" +
-"    $(document).ready(function() {\n" +
-"\n" +
-"        \n" +
-"\n" +
-"        var rawUsers = '[{\"login\":\"cberthier\",\"password\":\"test1\"},{\"login\":\"mgontier\",\"password\":\"test2\"},{\"login\":\"smacky\",\"password\":\"test3\"},{\"login\":\"mjackson\",\"password\":\"test4\"},{\"login\":\"rmoreno\",\"password\":\"test5\"},{\"login\":\"bfa\",\"password\":\"test6\"},{\"login\":\"mhollande\",\"password\":\"test7\"},{\"login\":\"palois\",\"password\":\"test8\"}]';\n" +
-"        var users = JSON.parse(rawUsers);\n" +
-"        \n" +
-"\n" +
-"\n" +
-"        $('#submitBtn').click(function() {\n" +
-"\n" +
-"\n" +
-"            var mailInput = $('#mail').val();\n" +
-"            var passInput = $('#pass').val();\n" +
-"\n" +
-"\n" +
-"            if (mailInput == '' && passInput == '') {\n" +
-"                alert('Login et mot de passe vides');\n" +
-"              \n" +
-"            } else if(mailInput == '') {\n" +
-"                alert('Login vide');\n" +
-"            }\n" +
-"            else if(passInput == '') {\n" +
-"                alert('Mot de passe vide');\n" +
-"            }\n" +
-"            else {\n" +
-"\n" +
-"                var isAuthentificated = false;\n" +
-"\n" +
-"                for (let i = 0; i < users.length; i++) {\n" +
-"\n" +
-"                    if (users[i].login == mailInput && users[i].password == passInput) {\n" +
-"                        isAuthentificated = true;\n" +
-"                    }\n" +
-"\n" +
-"                }\n" +
-"\n" +
-"               if(isAuthentificated == true) { \n" +
-"                   window.location.replace(\"./comptes/\"+mailInput+\"/index.html\");\n" +
-"                }  \n" +
-"               else alert('Le mot de passe ou le mail ne sont pas correct.');\n" +
-"\n" +
-"            }\n" +
-"\n" +
-"            \n" +
-"        \n" +
-"        });\n" +
-"    });\n" +
-"</script>");
+"<script src=\"main.js\"></script>");
         bw.close();
         } catch (IOException e) {
             e.printStackTrace();
